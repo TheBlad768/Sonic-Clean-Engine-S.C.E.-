99bb128d09297d1ec87b5ea3319881979a7779de
TheBlad768 <master.metalsonic2014@gmail.com>
2025-05-07 07:02:14 +0400

    Comments update

--- Objects/Main/Animals/Animals.asm
+++ Objects/Main/Animals/Animals.asm
@@ -374,9 +374,9 @@ Obj_Animal_ChkDel:
 ; ---------------------------------------------------------------------------
 
 .offscreen
-		move.w	respawn_addr(a0),d0
-		beq.s	.delete
-		movea.w	d0,a2
+		move.w	respawn_addr(a0),d0									; get address in respawn table
+		beq.s	.delete												; if it's zero, it isn't remembered
+		movea.w	d0,a2												; load address into a2
 		bclr	#7,(a2)
 
 .delete
--- Objects/Main/Invisible Blocks/Invisible Block(Solid).asm
+++ Objects/Main/Invisible Blocks/Invisible Block(Solid).asm
@@ -45,9 +45,9 @@ Obj_Invisible_SolidBlock:
 ; ---------------------------------------------------------------------------
 
 .offscreen
-		move.w	respawn_addr(a0),d0
-		beq.s	.delete
-		movea.w	d0,a2
+		move.w	respawn_addr(a0),d0						; get address in respawn table
+		beq.s	.delete									; if it's zero, it isn't remembered
+		movea.w	d0,a2									; load address into a2
 		bclr	#7,(a2)
 
 .delete
