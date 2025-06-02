f3551d8ac993c31276dbc04e4c949b3cddf000b9
TheBlad768 <master.metalsonic2014@gmail.com>
2025-06-02 06:40:46 +0400

    space inside tab fix 2

--- Levels/DEZ/Pointers/DEZ3 - Pointers.asm
+++ Levels/DEZ/Pointers/DEZ3 - Pointers.asm
@@ -24,14 +24,14 @@
 
 		; Level 1st 8x8 data, 2nd 8x8 data, Blocks pointer, 1st 16x16 data, 2nd 16x16 data, Chunks pointer, 1st 128x128 data, 2nd 128x128 data, Palette, Water palette, Music
 		levartptrs \
- 		DEZ_8x8_KosPM, \
- 		0, \
- 		DEZ_16x16_Unc, \
- 		0, \
- 		0, \
- 		Chunk_table, \
- 		DEZ_128x128_KosP, \
- 		0, \
+		DEZ_8x8_KosPM, \
+		0, \
+		DEZ_16x16_Unc, \
+		0, \
+		0, \
+		Chunk_table, \
+		DEZ_128x128_KosP, \
+		0, \
 		PalID_DEZ, \
 		PalID_WaterDEZ, \
 		mus_DEZ1
--- Objects/Players/Sonic/Sonic.asm
+++ Objects/Players/Sonic/Sonic.asm
@@ -516,13 +516,13 @@ loc_11056:
 
 Sonic_ChgFallAnim:
 		btst	#Status_Roll,status(a0)			; is Sonic rolling?
-		bne.s	.return					 	; if yes, branch
+		bne.s	.return						; if yes, branch
 		btst	#Status_OnObj,status(a0)			; is Sonic standing on an object?
-		bne.s	.return 						; if yes, branch
+		bne.s	.return						; if yes, branch
 		tst.b	flip_angle(a0)					; flip angle?
-		bne.s	.return 						; if yes, branch
+		bne.s	.return						; if yes, branch
 		tst.b	anim(a0)						; walk animation?
-		bne.s	.return 						; if not, branch
+		bne.s	.return						; if not, branch
 		moveq	#btnABC,d0					; read only A/B/C buttons
 		and.b	(Ctrl_1_pressed_logical).w,d0	; get button presses
 		beq.s	.return
@@ -2525,10 +2525,10 @@ loc_126DC:
 		add.w	d2,d2
 
 loc_1270A:
-		lea	SonAni_Run(pc),a1 	; use running	animation
+		lea	SonAni_Run(pc),a1	; use running	animation
 		cmpi.w	#$600,d2
 		bhs.s	loc_12724
-		lea	SonAni_Walk(pc),a1 	; use walking animation
+		lea	SonAni_Walk(pc),a1	; use walking animation
 		add.b	d0,d0
 
 loc_12724:
