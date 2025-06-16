36b100fa52518a0cf33881581bf4f7bafcb1d2e5
TheBlad768 <master.metalsonic2014@gmail.com>
2025-06-16 16:36:01 +0400

    Update DEZ1 - Debug List.asm

--- Levels/DEZ/Debug/DEZ1 - Debug List.asm
+++ Levels/DEZ/Debug/DEZ1 - Debug List.asm
@@ -14,12 +14,12 @@ Debug_DEZ1: dbglistheader
 	dbglistobj Obj_Monitor, Map_Monitor, 8, 9, ArtTile_Monitors, 0, 0
 	dbglistobj Obj_PathSwap, Map_PathSwap, 9, 1, ArtTile_Ring, 1, 0
 	dbglistobj Obj_PathSwap, Map_PathSwap, $D, 5, ArtTile_Ring, 1, 0
-	dbglistobj Obj_Spring, Map_Spring, $81, 0, $494, 0, 0
-	dbglistobj Obj_Spring, Map_Spring2, $82, 0, $494, 0, 0
-	dbglistobj Obj_Spring, Map_Spring, $90, 3, $4A0, 0, 0
-	dbglistobj Obj_Spring, Map_Spring2, $92, 3, $4A0, 0, 0
-	dbglistobj Obj_Spring, Map_Spring, $A0, 6, $494, 0, 0
-	dbglistobj Obj_Spring, Map_Spring2, $A2, 6, $494, 0, 0
+	dbglistobj Obj_Spring, Map_Spring, $81, 0, ArtTile_SpikesSprings+$10, 0, 0
+	dbglistobj Obj_Spring, Map_Spring2, $82, 0, ArtTile_SpikesSprings+$10, 0, 0
+	dbglistobj Obj_Spring, Map_Spring, $90, 3, ArtTile_SpikesSprings+$1C, 0, 0
+	dbglistobj Obj_Spring, Map_Spring2, $92, 3, ArtTile_SpikesSprings+$1C, 0, 0
+	dbglistobj Obj_Spring, Map_Spring, $A0, 6, ArtTile_SpikesSprings+$10, 0, 0
+	dbglistobj Obj_Spring, Map_Spring2, $A2, 6, ArtTile_SpikesSprings+$10, 0, 0
 	dbglistobj Obj_Spikes, Map_Spikes, 0, 0, ArtTile_SpikesSprings+8, 0, 0
 	dbglistobj Obj_Spikes, Map_Spikes, $40, 4, ArtTile_SpikesSprings, 0, 0
 	dbglistobj Obj_Spikebonker, Map_Spikebonker, $40, 0, $500, 0, 1
