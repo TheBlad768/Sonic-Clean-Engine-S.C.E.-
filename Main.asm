2fa763c0a968481994f7ec846a414e74b8bb894d
TheBlad768 <master.metalsonic2014@gmail.com>
2025-06-22 20:08:43 +0400

    Update Main.asm

--- Main.asm
+++ Main.asm
@@ -485,11 +485,19 @@ EndOfHeader
 
 	endif
 
-; ---------------------------------------------------------------
-; Error handling module
-; ---------------------------------------------------------------
+; --------------------------------------------------------------
+; Debugging modules
+; --------------------------------------------------------------
 
 		include "Engine/Debugger/ErrorHandler/ErrorHandler.asm"
+
+; ---------------------------------------------------------------
+; WARNING!
+;	DO NOT put any data from now on! DO NOT use ROM padding!
+;	Symbol data should be appended here after ROM is compiled
+;	by ConvSym utility, otherwise debugger modules won't be able
+;	to resolve symbol names.
+; ---------------------------------------------------------------
 
 ; end of 'ROM'
 EndOfROM:
