cde34e82afbc0b4f2e5348f0b680e0686915948e
TheBlad768 <master.metalsonic2014@gmail.com>
2025-06-25 22:40:30 +0400

    tab fix

--- Sound/Definitions.asm
+++ Sound/Definitions.asm
@@ -1,3 +1,3 @@
-	include "Sound/Macros.asm"
-	include "Sound/Constants.asm"
-	include "Sound/Sound IDs.asm"
+		include "Sound/Macros.asm"
+		include "Sound/Constants.asm"
+		include "Sound/Sound IDs.asm"
