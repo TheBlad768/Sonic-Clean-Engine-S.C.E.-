9f8bb2fc3763cca290877e9b00ae1884c99ad9ff
TheBlad768 <master.metalsonic2014@gmail.com>
2025-07-01 08:14:46 +0400

    batch update

--- build_debug.bat
+++ build_debug.bat
@@ -30,11 +30,11 @@ REM // '-L' listing to file
 REM // '-U' forces case-sensitivity
 REM // '-E' output errors to a file (*.log)
 REM // '-i .' allows (b)include paths to be absolute
-set AS_MSGPATH=Tools/AS/Windows
+set AS_MSGPATH=Tools\AS\Windows
 set USEANSI=n
 
 REM // Allow the user to choose to print error messages out by supplying the -pe parameter
-"%AS_MSGPATH%/asw.exe" -xx -n -q -c -D __DEBUG__ -olist Main.Debug.lst -A -L -U -E -i . Main.asm
+"%AS_MSGPATH%\asw.exe" -xx -n -q -c -D __DEBUG__ -olist Main.Debug.lst -A -L -U -E -i . Main.asm
 IF ERRORLEVEL 1 (
     echo Assembler failed to execute.
     goto LABLERROR5
@@ -45,7 +45,7 @@ IF NOT EXIST Main.p (
 )
 
 REM // Convert the assembled file to binary
-"%AS_MSGPATH%/p2bin.exe" -p=FF -z=0,kosinskiplus,Size_of_DAC_driver_guess,after Main.p S3CE.Debug.gen Main.h
+"%AS_MSGPATH%\p2bin.exe" -p=FF -z=0,kosinskiplus,Size_of_DAC_driver_guess,after Main.p S3CE.Debug.gen Main.h
 IF ERRORLEVEL 1 (
     echo Failed to convert Main.p to S3CE.Debug.gen.
     pause & exit /b 1
@@ -74,22 +74,22 @@ IF NOT EXIST S3CE.Debug.gen (
 )
 
 REM // Generate debug information
-"%AS_MSGPATH%/convsym.exe" Main.lst S3CE.Debug.gen -input as_lst -range 0 FFFFFF -exclude -filter \"z[A-Z].+\" -a
+"%AS_MSGPATH%\convsym.exe" Main.lst S3CE.Debug.gen -input as_lst -range 0 FFFFFF -exclude -filter \"z[A-Z].+\" -a
 IF ERRORLEVEL 1 (
     echo Failed to generate debug information for S3CE.Debug.gen.
     pause & exit /b 1
 )
-"%AS_MSGPATH%/convsym.exe" Main.lst "Engine/_RAM.Debug.asm" -in as_lst -out asm -range FF0000 FFFFFF
+"%AS_MSGPATH%\convsym.exe" Main.lst "Engine\_RAM.Debug.asm" -in as_lst -out asm -range FF0000 FFFFFF
 IF ERRORLEVEL 1 (
-    echo Failed to generate debug information for Engine/_RAM.Debug.asm.
+    echo Failed to generate debug information for _RAM.Debug.asm.
     pause & exit /b 1
 )
 
 REM // Make ROM padding (commented out as in the original)
-REM // "%AS_MSGPATH%/rompad.exe" S3CE.Debug.gen 255 0
+REM // "%AS_MSGPATH%\rompad.exe" S3CE.Debug.gen 255 0
 
 REM // Fix the ROM header (checksum)
-"%AS_MSGPATH%/fixheader.exe" S3CE.Debug.gen
+"%AS_MSGPATH%\fixheader.exe" S3CE.Debug.gen
 IF ERRORLEVEL 1 (
     echo Failed to fix the ROM header for S3CE.Debug.gen.
     pause & exit /b 1
