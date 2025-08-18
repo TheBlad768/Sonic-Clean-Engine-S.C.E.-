7fde2db7c2ce0160b4bbfb1203f5cd8a2be034bc
TheBlad768 <master.metalsonic2014@gmail.com>
2025-08-18 19:20:49 +0400

    RAM listing

--- build_debug.sh
+++ build_debug.sh
@@ -28,7 +28,7 @@ rm -f S3CE.debug.h
 
 # Generate debug information
 ${AS_MSGPATH}/convsym S3CE.debug.lst S3CE.debug.gen -input as_lst -range 0 FFFFFF -exclude -filter \"z[A-Z].+\" -a
-${AS_MSGPATH}/convsym S3CE.debug.lst "Engine/_RAM.debug.asm" -in as_lst -out asm -range FF0000 FFFFFF
+${AS_MSGPATH}/convsym S3CE.debug.lst "Engine/_RAM.debug.lst" -in as_lst -out asm -range FF0000 FFFFFF
 
 # Make ROM padding (commented out as in the original)
 #${AS_MSGPATH}/rompad S3CE.debug.gen 255 0
