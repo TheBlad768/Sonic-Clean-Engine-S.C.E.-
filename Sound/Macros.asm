46075a05e6daf2f17c4c0b663dd07af3d8419214
TheBlad768 <master.metalsonic2014@gmail.com>
2025-09-02 14:13:00 +0400

    Move music/sfx macro in sound macro

--- Sound/Macros.asm
+++ Sound/Macros.asm
@@ -1,15 +1,86 @@
 ; ---------------------------------------------------------------------------
+; play a sound effect or music
+; input: track, terminate routine, branch or jump, move operand size
+; ---------------------------------------------------------------------------
+
+music macro track,terminate,byte
+    if ("byte"="0") || ("byte"="")
+	moveq	#signextendB(track),d0
+    else
+	move.w	#(track),d0
+    endif
+    if ("terminate"="0") || ("terminate"="")
+	jsr	(Play_Music).w
+    else
+	jmp	(Play_Music).w
+    endif
+    endm
+
+sfx macro track,terminate,byte
+    if ("byte"="0") || ("byte"="")
+	moveq	#signextendB(track),d0
+    else
+	move.w	#(track),d0
+    endif
+    if ("terminate"="0") || ("terminate"="")
+	jsr	(Play_SFX).w
+    else
+	jmp	(Play_SFX).w
+    endif
+    endm
+
+sfxcont macro track,wait,terminate,byte
+    if ("byte"="0") || ("byte"="")
+	moveq	#signextendB(track),d0
+    else
+	move.w	#(track),d0
+    endif
+	moveq	#signextendB(wait),d1
+    if ("terminate"="0") || ("terminate"="")
+	jsr	(Play_SFX_Continuous).w
+    else
+	jmp	(Play_SFX_Continuous).w
+    endif
+    endm
+
+tempo macro speed,terminate,byte
+    if ("byte"="0") || ("byte"="")
+	moveq	#signextendB(speed),d0
+    else
+	move.w	#(speed),d0
+    endif
+    if ("terminate"="0") || ("terminate"="")
+	jsr	(Change_Music_Tempo).w
+    else
+	jmp	(Change_Music_Tempo).w
+    endif
+    endm
+
+sample macro id,terminate,byte
+    if ("byte"="0") || ("byte"="")
+	moveq	#signextendB(id),d0
+    else
+	move.w	#(id),d0
+    endif
+    if ("terminate"="0") || ("terminate"="")
+	jsr	(Play_Sample).w
+    else
+	jmp	(Play_Sample).w
+    endif
+    endm
+
+; ---------------------------------------------------------------------------
 ; stop the Z80
 ; ---------------------------------------------------------------------------
 
 SMPS_stopZ80 macro
 	SMPS_stopZ80a
 	SMPS_waitZ80
-	endm
+    endm
 
 SMPS_stopZ80a macro
 	move.w	#$100,(Z80_bus_request).l
-	endm
+    endm
 
 ; ---------------------------------------------------------------------------
 ; wait for Z80 to stop
@@ -20,7 +91,7 @@ SMPS_waitZ80 macro
 .wait
 	btst	#0,(Z80_bus_request).l
 	bne.s	.wait
-	endm
+    endm
 
 ; ---------------------------------------------------------------------------
 ; reset the Z80
@@ -28,11 +99,11 @@ SMPS_waitZ80 macro
 
 SMPS_resetZ80 macro
 	move.w	#$100,(Z80_reset).l
-	endm
+    endm
 
 SMPS_resetZ80a macro
 	move.w	#0,(Z80_reset).l
-	endm
+    endm
 
 ; ---------------------------------------------------------------------------
 ; start the Z80
@@ -40,7 +111,7 @@ SMPS_resetZ80a macro
 
 SMPS_startZ80 macro
 	move.w	#0,(Z80_bus_request).l		; start the Z80
-	endm
+    endm
 
 ; ---------------------------------------------------------------------------
 ; Pause music
@@ -50,7 +121,7 @@ SMPS_PauseMusic macro
 	SMPS_stopZ80
 	move.b	#1,(Z80_RAM+zPauseFlag).l	; pause the music
 	SMPS_startZ80
-	endm
+    endm
 
 ; ---------------------------------------------------------------------------
 ; Unpause music
@@ -60,4 +131,4 @@ SMPS_UnpauseMusic macro
 	SMPS_stopZ80
 	move.b	#$80,(Z80_RAM+zPauseFlag).l	; unpause music
 	SMPS_startZ80
-	endm
+    endm
