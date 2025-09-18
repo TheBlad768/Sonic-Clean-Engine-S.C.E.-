1b33abeba7425ef86617483bc4f52db326f3ddb7
TheBlad768 <master.metalsonic2014@gmail.com>
2025-09-18 05:25:55 +0400

    Comments update

--- Engine/Core/Interrupt Handler.asm
+++ Engine/Core/Interrupt Handler.asm
@@ -24,6 +24,8 @@ VInt:
 		; detect PAL region consoles
 		btst	#0,(VDP_control_port-VDP_control_port)+1(a5)
 		beq.s	.notpal								; branch if it's not a PAL system
+
+		; wait to hide CRAM dots
 		move.w	#$700,d0
 		dbf	d0,*								; otherwise, waste a bit of time here
 
@@ -76,6 +78,8 @@ VInt_Lag_Level:
 		; detect PAL region consoles
 		btst	#0,(VDP_control_port-VDP_control_port)+1(a5)
 		beq.s	.notpal								; branch if it isn't a PAL system
+
+		; wait to hide CRAM dots
 		move.w	#$700,d0
 		dbf	d0,*								; otherwise waste a bit of time here
 
@@ -103,6 +107,8 @@ VInt_Lag_NoWater:
 		; detect PAL region consoles
 		btst	#0,(VDP_control_port-VDP_control_port)+1(a5)
 		beq.s	.notpal								; branch if it isn't a PAL system
+
+		; wait to hide CRAM dots
 		move.w	#$700,d0
 		dbf	d0,*								; otherwise, waste a bit of time here
 
