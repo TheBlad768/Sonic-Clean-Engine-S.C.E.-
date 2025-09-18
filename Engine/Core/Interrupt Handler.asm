1db0ab94403a315557defac1e74412653dd7d1f8
TheBlad768 <master.metalsonic2014@gmail.com>
2025-09-18 08:49:59 +0400

    VRAM_Horiz_Scroll_Table_Size

--- Engine/Core/Interrupt Handler.asm
+++ Engine/Core/Interrupt Handler.asm
@@ -186,7 +186,7 @@ Do_ControllerPal:
 
 .skipwater
 		dma68kToVDP Sprite_table_buffer,VRAM_Sprite_Attribute_Table,VRAM_Sprite_Attribute_Table_Size,VRAM
-		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,(224<<2),VRAM
+		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,VRAM_Horiz_Scroll_Table_Size,VRAM
 		jsr	(Process_DMA_Queue).w
 		startZ80
 		rts
@@ -204,7 +204,7 @@ VInt_LevelSelect:
 		startZ802
 		dma68kToVDP Normal_palette,0,$80,CRAM
 		dma68kToVDP Sprite_table_buffer,VRAM_Sprite_Attribute_Table,VRAM_Sprite_Attribute_Table_Size,VRAM
-		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,(224<<2),VRAM
+		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,VRAM_Horiz_Scroll_Table_Size,VRAM
 		dma68kToVDP (LevelSelect_buffer2),VRAM_Plane_A_Name_Table,VRAM_Plane_Table_Size,VRAM	; foreground buffer to VRAM
 		jsr	(Process_DMA_Queue).w
 		startZ80
@@ -306,7 +306,7 @@ VInt_Level_NoNegativeFlash:
 		move.w	(H_int_counter_command).w,VDP_control_port-VDP_control_port(a5)
 
 VInt_Level_Cont:
-		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,(224<<2),VRAM
+		dma68kToVDP H_scroll_buffer,VRAM_Horiz_Scroll_Table,VRAM_Horiz_Scroll_Table_Size,VRAM
 		dma68kToVDP Sprite_table_buffer,VRAM_Sprite_Attribute_Table,VRAM_Sprite_Attribute_Table_Size,VRAM
 		jsr	(Process_DMA_Queue).w
 		bsr.s	VInt_SpecialFunction
