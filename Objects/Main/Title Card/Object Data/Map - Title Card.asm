dcc291e3d4063865e3dbd9b3dd54859c6a05f073
TheBlad768 <master.metalsonic2014@gmail.com>
2025-09-20 06:06:34 +0400

    Update Map - Title Card.asm

--- Objects/Main/Title Card/Object Data/Map - Title Card.asm
+++ Objects/Main/Title Card/Object Data/Map - Title Card.asm
@@ -4,11 +4,11 @@
 
 Map_TitleCard:
 		dc.w 0						; null
-		dc.w Map_TitleCardRBanner-Map_TitleCard		; red banner
-		dc.w Map_TitleCardAct-Map_TitleCard		; ACT
-		dc.w Map_TitleCardZone-Map_TitleCard		; ZONE
-		dc.w Map_TitleCardDEZ-Map_TitleCard		; DEATH EGG
-Map_TitleCardRBanner:
+		dc.w Map_TitleCard_Banner-Map_TitleCard		; red banner
+		dc.w Map_TitleCard_Act-Map_TitleCard		; ACT
+		dc.w Map_TitleCard_Zone-Map_TitleCard		; ZONE
+		dc.w Map_TitleCard_DEZ-Map_TitleCard		; DEATH EGG
+Map_TitleCard_Banner:
 		dc.w $10
 		dc.b $58, 9, $80, $10, $FF, $E8
 		dc.b $58, 9, $80, $16, 0, 0
@@ -26,17 +26,17 @@ Map_TitleCardRBanner:
 		dc.b $30, $F, $80, 0, 0, 0
 		dc.b $50, $F, $80, 0, $FF, $E0
 		dc.b $50, $F, $80, 0, 0, 0
-Map_TitleCardAct:
+Map_TitleCard_Act:
 		dc.w 2
 		dc.b $10, 9, $80, $37, $FF, $E4
 		dc.b 0, $F, $80, $3D, $FF, $F5
-Map_TitleCardZone:
+Map_TitleCard_Zone:
 		dc.w 4
 		dc.b 0, 6, $80, $31, $FF, $DC			; Z
 		dc.b 0, $A, $80, $28, $FF, $EC			; O
 		dc.b 0, 6, $80, $22, 0, 4			; N
 		dc.b 0, 6, $80, $1C, 0, $14			; E
-Map_TitleCardDEZ:
+Map_TitleCard_DEZ:
 		dc.w 8
 		dc.b 0, 6, $80, $53, $FF, $F8			; D
 		dc.b 0, 6, $80, $1C, 0, 8			; E
