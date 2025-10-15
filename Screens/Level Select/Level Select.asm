d1abdb9921b546f565621e4d057f0ec26faea475
TheBlad768 <master.metalsonic2014@gmail.com>
2025-10-15 22:04:45 +0400

    Level Select fix
    Some flags were not cleared, and this caused errors that I only noticed now
    
    - Comments update

--- Screens/Level Select/Level Select.asm
+++ Screens/Level Select/Level Select.asm
@@ -63,6 +63,8 @@ LevelSelectScreen:
 		; clear
 		move.b	d0,(Water_full_screen_flag).w
 		move.b	d0,(Water_flag).w
+		move.b	d0,(HUD_RAM.status).w
+		move.b	d0,(Update_HUD_timer).w								; clear time counter update flag
 		move.w	d0,(Current_zone_and_act).w
 		move.w	d0,(Apparent_zone_and_act).w
 		move.b	d0,(Last_star_post_hit).w
