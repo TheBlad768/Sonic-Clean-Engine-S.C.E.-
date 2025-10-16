04e07afc2878f6e56765f9904cc1ce4ebdab83db
TheBlad768 <master.metalsonic2014@gmail.com>
2025-10-16 08:29:34 +0400

    UniVoiceBank moved

--- Sound/Engine/Sonic 2 Clone Driver v2.asm
+++ Sound/Engine/Sonic 2 Clone Driver v2.asm
@@ -3643,7 +3643,7 @@ cfChanFMCommand:
 ; FM Universal Voice Bank
 ; ---------------------------------------------------------------------------
     if SMPS_EnableUniversalVoiceBank
-	include "Sound/FM Universal Voice Bank.asm"
+UniVoiceBank:	include "Sound/FM Universal Voice Bank.asm"
     endif
 ; ---------------------------------------------------------------------------
 ; PSG volume envelopes 'include's and pointers
