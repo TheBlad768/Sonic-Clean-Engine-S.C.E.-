eb08b9f29b151496b9e600e4ce986dc1b7f13895
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-05 09:11:47 +0400

    Display text on the plane variables

--- Engine/Variables.asm
+++ Engine/Variables.asm
@@ -42,9 +42,9 @@ V_scroll_buffer_end					= *
 ; table variables
 Collision_response_list:				ds.w $80/2			; only objects in this list are processed by the collision response routines
 Pos_table:						ds.l 64				; recorded player xy position buffer
-Ring_status_table:					ds.w RingsTable_Size		; ring status table(1 word)
+Ring_status_table:					ds.w RingsTable_Size		; ring status table (1 word)
 Ring_status_table_end					= *
-Object_respawn_table:					ds.b ObjectsTable_Size		; object respawn table(1 byte)
+Object_respawn_table:					ds.b ObjectsTable_Size		; object respawn table (1 byte)
 Object_respawn_table_end				= *
 Sprite_table_buffer:					ds.b 80*8
 Sprite_table_buffer_end					= *
