fae46408d4e61451bd8b1f5a101babd71786f80f
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-12 15:54:33 +0400

    Macro for a pattern load request update

--- Data/Pattern Load Cues.asm
+++ Data/Pattern Load Cues.asm
@@ -11,7 +11,7 @@ PLC1_Sonic: plrlistheader
 		plreq ArtTile_StarPost, ArtKosPM_EnemyPtsStarPost			; starpost
 		plreq ArtTile_Ring_Sparks, ArtKosPM_Ring_Sparks				; rings
 		plreq ArtTile_HUD, ArtKosPM_HUD						; HUD
-PLC1_Sonic_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues 2 - Sonic
@@ -21,7 +21,7 @@ PLC2_Sonic: plrlistheader
 		plreq ArtTile_SpikesSprings, ArtKosPM_SpikesSprings			; spikes and normal spring
 		plreq ArtTile_Monitors, ArtKosPM_Monitors				; monitors
 		plreq ArtTile_Explosion, ArtKosPM_Explosion				; explosion
-PLC2_Sonic_end
+		plrlistend
 
 ; ===========================================================================
 ; Level pattern load cues
@@ -34,7 +34,7 @@ PLC2_Sonic_end
 
 PLC1_DEZ1_Before: plrlistheader
 		plreq $47E, ArtKosPM_GrayButton						; button
-PLC1_DEZ1_Before_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (After)
@@ -42,7 +42,7 @@ PLC1_DEZ1_Before_end
 
 PLC2_DEZ1_After: plrlistheader
 		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
-PLC2_DEZ1_After_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (Before)
@@ -50,7 +50,7 @@ PLC2_DEZ1_After_end
 
 PLC1_DEZ2_Before: plrlistheader
 		plreq $47E, ArtKosPM_GrayButton						; button
-PLC1_DEZ2_Before_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (After)
@@ -58,7 +58,7 @@ PLC1_DEZ2_Before_end
 
 PLC2_DEZ2_After: plrlistheader
 		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
-PLC2_DEZ2_After_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (Before)
@@ -66,7 +66,7 @@ PLC2_DEZ2_After_end
 
 PLC1_DEZ3_Before: plrlistheader
 		plreq $47E, ArtKosPM_GrayButton						; button
-PLC1_DEZ3_Before_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (After)
@@ -74,7 +74,7 @@ PLC1_DEZ3_Before_end
 
 PLC2_DEZ3_After: plrlistheader
 		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
-PLC2_DEZ3_After_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (Before)
@@ -82,7 +82,7 @@ PLC2_DEZ3_After_end
 
 PLC1_DEZ4_Before: plrlistheader
 		plreq $47E, ArtKosPM_GrayButton						; button
-PLC1_DEZ4_Before_end
+		plrlistend
 
 ; ===========================================================================
 ; Pattern load cues - Death Egg Zone (After)
@@ -90,7 +90,7 @@ PLC1_DEZ4_Before_end
 
 PLC2_DEZ4_After: plrlistheader
 		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
-PLC2_DEZ4_After_end
+		plrlistend
 
 ; ===========================================================================
 ; Level pattern load cues
@@ -104,4 +104,4 @@ PLC2_DEZ4_After_end
 PLCAnimals_DEZ1: plrlistheader
 		plreq $580, ArtKosPM_BlueFlicky
 		plreq $592, ArtKosPM_Chicken
-PLCAnimals_DEZ1_end
+		plrlistend
