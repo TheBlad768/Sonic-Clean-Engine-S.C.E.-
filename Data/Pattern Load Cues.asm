d3044501b954149cfaafe557fedd8cedb6ab77cc
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-12 14:29:53 +0400

    Update Pattern Load Cues.asm

--- Data/Pattern Load Cues.asm
+++ Data/Pattern Load Cues.asm
@@ -8,9 +8,9 @@
 ; ===========================================================================
 
 PLC1_Sonic: plrlistheader
-		plreq ArtTile_StarPost, ArtKosPM_EnemyPtsStarPost		; starpost
-		plreq ArtTile_Ring_Sparks, ArtKosPM_Ring_Sparks			; rings
-		plreq ArtTile_HUD, ArtKosPM_HUD					; HUD
+		plreq ArtTile_StarPost, ArtKosPM_EnemyPtsStarPost			; starpost
+		plreq ArtTile_Ring_Sparks, ArtKosPM_Ring_Sparks				; rings
+		plreq ArtTile_HUD, ArtKosPM_HUD						; HUD
 PLC1_Sonic_end
 
 ; ===========================================================================
@@ -18,9 +18,9 @@ PLC1_Sonic_end
 ; ===========================================================================
 
 PLC2_Sonic: plrlistheader
-		plreq ArtTile_SpikesSprings, ArtKosPM_SpikesSprings		; spikes and normal spring
-		plreq ArtTile_Monitors, ArtKosPM_Monitors			; monitors
-		plreq ArtTile_Explosion, ArtKosPM_Explosion			; explosion
+		plreq ArtTile_SpikesSprings, ArtKosPM_SpikesSprings			; spikes and normal spring
+		plreq ArtTile_Monitors, ArtKosPM_Monitors				; monitors
+		plreq ArtTile_Explosion, ArtKosPM_Explosion				; explosion
 PLC2_Sonic_end
 
 ; ===========================================================================
@@ -29,67 +29,67 @@ PLC2_Sonic_end
 ; ===========================================================================
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (Before)
+; Pattern load cues - Death Egg Zone (Before)
 ; ===========================================================================
 
 PLC1_DEZ1_Before: plrlistheader
-		plreq $47E, ArtKosPM_GrayButton					; button
+		plreq $47E, ArtKosPM_GrayButton						; button
 PLC1_DEZ1_Before_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (After)
+; Pattern load cues - Death Egg Zone (After)
 ; ===========================================================================
 
 PLC2_DEZ1_After: plrlistheader
-		plreq $500, ArtKosPM_Spikebonker				; spikebonker badnik
+		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
 PLC2_DEZ1_After_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (Before)
+; Pattern load cues - Death Egg Zone (Before)
 ; ===========================================================================
 
 PLC1_DEZ2_Before: plrlistheader
-		plreq $47E, ArtKosPM_GrayButton					; button
+		plreq $47E, ArtKosPM_GrayButton						; button
 PLC1_DEZ2_Before_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (After)
+; Pattern load cues - Death Egg Zone (After)
 ; ===========================================================================
 
 PLC2_DEZ2_After: plrlistheader
-		plreq $500, ArtKosPM_Spikebonker				; spikebonker badnik
+		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
 PLC2_DEZ2_After_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (Before)
+; Pattern load cues - Death Egg Zone (Before)
 ; ===========================================================================
 
 PLC1_DEZ3_Before: plrlistheader
-		plreq $47E, ArtKosPM_GrayButton					; button
+		plreq $47E, ArtKosPM_GrayButton						; button
 PLC1_DEZ3_Before_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (After)
+; Pattern load cues - Death Egg Zone (After)
 ; ===========================================================================
 
 PLC2_DEZ3_After: plrlistheader
-		plreq $500, ArtKosPM_Spikebonker				; spikebonker badnik
+		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
 PLC2_DEZ3_After_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (Before)
+; Pattern load cues - Death Egg Zone (Before)
 ; ===========================================================================
 
 PLC1_DEZ4_Before: plrlistheader
-		plreq $47E, ArtKosPM_GrayButton					; button
+		plreq $47E, ArtKosPM_GrayButton						; button
 PLC1_DEZ4_Before_end
 
 ; ===========================================================================
-; Pattern load cues - Death Egg (After)
+; Pattern load cues - Death Egg Zone (After)
 ; ===========================================================================
 
 PLC2_DEZ4_After: plrlistheader
-		plreq $500, ArtKosPM_Spikebonker				; spikebonker badnik
+		plreq $500, ArtKosPM_Spikebonker					; spikebonker badnik
 PLC2_DEZ4_After_end
 
 ; ===========================================================================
