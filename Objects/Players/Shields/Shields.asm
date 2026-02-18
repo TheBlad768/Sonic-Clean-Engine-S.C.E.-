39c5c2a8bfa7df5a5164a7926aead27cbff80f39
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-18 17:11:34 +0400

    Update Shields.asm

--- Objects/Players/Shields/Shields.asm
+++ Objects/Players/Shields/Shields.asm
@@ -21,7 +21,7 @@ Obj_FireShield:
 
 .nothighpriority
 		move.w	#1,anim(a0)							; clear anim and set prev_anim to 1
-		st	objoff_3A(a0)							; reset objoff_3A (used by Perform_DPLC)
+		st	ros_prev_frame(a0)						; reset objoff_3A (used by Perform_DPLC)
 
 .main
 		lea	(Player_1).w,a2							; a2=character
@@ -112,7 +112,7 @@ Obj_LightningShield:
 
 .nothighpriority
 		move.w	#1,anim(a0)							; clear anim and set prev_anim to 1
-		st	objoff_3A(a0)							; reset objoff_3A (used by Perform_DPLC)
+		st	ros_prev_frame(a0)						; reset objoff_3A (used by Perform_DPLC)
 
 .main
 		lea	(Player_1).w,a2							; a2=character
@@ -290,7 +290,7 @@ Obj_BubbleShield:
 
 .nothighpriority
 		move.w	#1,anim(a0)							; clear anim and set prev_anim to 1
-		st	objoff_3A(a0)							; reset objoff_3A (used by Perform_DPLC)
+		st	ros_prev_frame(a0)						; reset objoff_3A (used by Perform_DPLC)
 		lea	(Player_1).w,a1							; a1=character
 		jsr	(Player_ResetAirTimer).l
 
@@ -360,7 +360,7 @@ Obj_InstaShield:
 
 .nothighpriority
 		move.w	#1,anim(a0)							; clear anim and set prev_anim to 1
-		st	objoff_3A(a0)							; reset objoff_3A (used by Perform_DPLC)
+		st	ros_prev_frame(a0)						; reset objoff_3A (used by Perform_DPLC)
 
 .main
 		lea	(Player_1).w,a2							; a2=character
