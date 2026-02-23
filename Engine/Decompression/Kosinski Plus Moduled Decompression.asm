29648dc323f199bec59b415d83f24f05598303c6
TheBlad768 <master.metalsonic2014@gmail.com>
2026-02-23 09:46:17 +0400

    tab fix 2

--- Engine/Decompression/Kosinski Plus Moduled Decompression.asm
+++ Engine/Decompression/Kosinski Plus Moduled Decompression.asm
@@ -14,15 +14,15 @@
 Queue_KosPlus:
 		move.w	(KosPlus_decomp_queue_count).w,d0
 		addq.w	#1,(KosPlus_decomp_queue_count).w
-		lsl.w	#3,d0											; multiply by 8
+		lsl.w	#3,d0								; multiply by 8
 		lea	(KosPlus_decomp_queue).w,a3
 		adda.w	d0,a3
 
 		; RaiseError is only available in DEBUG builds
-		ifdebug	jsr	(Queue_KosPlus_RaiseError).l							; raise an error if there is kosinski plus buffer overflow
+		ifdebug	jsr	(Queue_KosPlus_RaiseError).l				; raise an error if there is kosinski plus buffer overflow
 
-		move.l	a1,(a3)+										; store source
-		move.l	a2,(a3)+										; store destination
+		move.l	a1,(a3)+							; store source
+		move.l	a2,(a3)+							; store destination
 		rts
 
 ; ---------------------------------------------------------------------------
@@ -36,8 +36,8 @@ LoadPLC_Raw_KosPlus:
 		bmi.s	.Done
 
 .queuePieces
-		movea.l	(a5)+,a1										; store source address
-		movea.l	(a5)+,a2										; store destination RAM address
+		movea.l	(a5)+,a1							; store source address
+		movea.l	(a5)+,a2							; store destination RAM address
 		bsr.s	Queue_KosPlus
 		dbf	d6,.queuePieces
 
@@ -60,8 +60,8 @@ Process_KosPlus_Module_Queue:
 		; set
 		movea.l	(KosPlus_module_queue).w,a1
 		lea	(KosPlus_decomp_buffer).w,a2
-		bsr.s	Queue_KosPlus										; add current module to decompression queue
-		ori.w	#$8000,(KosPlus_modules_left).w								; and set bit to signify decompression in progress
+		bsr.s	Queue_KosPlus							; add current module to decompression queue
+		ori.w	#$8000,(KosPlus_modules_left).w					; and set bit to signify decompression in progress
 
 .Done
 		rts
@@ -69,13 +69,13 @@ Process_KosPlus_Module_Queue:
 
 .DecompressionStarted
 		tst.w	(KosPlus_decomp_queue_count).w
-		bne.s	.Done											; branch if the decompression isn't complete
+		bne.s	.Done								; branch if the decompression isn't complete
 
 		; otherwise, DMA the decompressed data to VRAM
 		andi.w	#$7F,(KosPlus_modules_left).w
 		move.w	#$1000/2,d3
 		subq.w	#1,(KosPlus_modules_left).w
-		bne.s	.Skip											; branch if it isn't the last module
+		bne.s	.Skip								; branch if it isn't the last module
 		move.w	(KosPlus_last_module_size).w,d3
 
 .Skip
@@ -83,19 +83,19 @@ Process_KosPlus_Module_Queue:
 		move.w	d2,d0
 		add.w	d3,d0
 		add.w	d3,d0
-		move.w	d0,(KosPlus_module_destination).w							; set new destination
-		move.l	(KosPlus_decomp_queue).w,(KosPlus_module_queue).w					; set new source
+		move.w	d0,(KosPlus_module_destination).w				; set new destination
+		move.l	(KosPlus_decomp_queue).w,(KosPlus_module_queue).w		; set new source
 		move.l	#dmaSource(KosPlus_decomp_buffer),d1
 		disableIntsSave
 		bsr.w	Add_To_DMA_Queue
 		enableIntsSave
 		tst.w	(KosPlus_modules_left).w
-		bne.s	.Done											; return if this wasn't the last module
+		bne.s	.Done								; return if this wasn't the last module
 		lea	(KosPlus_module_queue).w,a0
 		lea	(KosPlus_module_queue+6).w,a1
 
 	rept bytesToXcnt(KosPlus_module_queue_end-KosPlus_module_queue,8)
-		move.l	(a1)+,(a0)+										; otherwise, shift all entries up
+		move.l	(a1)+,(a0)+							; otherwise, shift all entries up
 		move.l	(a1)+,(a0)+
 	endr
 
@@ -105,10 +105,10 @@ Process_KosPlus_Module_Queue:
 
 		; clear last slot
 		moveq	#0,d0
-		move.l	d0,(a0)+										; and mark the last slot as free
+		move.l	d0,(a0)+							; and mark the last slot as free
 		move.w	d0,(a0)+
 		move.l	(KosPlus_module_queue).w,d0
-		beq.s	LoadPLC_Raw_KosPlusM.Done								; return if the queue is now empty
+		beq.s	LoadPLC_Raw_KosPlusM.Done					; return if the queue is now empty
 		movea.l	d0,a1
 		move.w	(KosPlus_module_destination).w,d2
 		bra.s	Process_KosPlus_Module_Queue_Init
@@ -124,8 +124,8 @@ LoadPLC_Raw_KosPlusM:
 		bmi.s	.Done
 
 .queuePieces
-		movea.l	(a5)+,a1										; store source address
-		move.w	(a5)+,d2										; store destination VRAM address
+		movea.l	(a5)+,a1							; store source address
+		move.w	(a5)+,d2							; store destination VRAM address
 		bsr.s	Queue_KosPlus_Module
 		dbf	d6,.queuePieces
 
@@ -147,19 +147,19 @@ LoadPLC_Raw_KosPlusM:
 
 Queue_KosPlus_Module:
 		lea	(KosPlus_module_queue).w,a2
-		tst.l	(a2)											; is the first slot free?
-		beq.s	Process_KosPlus_Module_Queue_Init							; if it is, branch
+		tst.l	(a2)								; is the first slot free?
+		beq.s	Process_KosPlus_Module_Queue_Init				; if it is, branch
 
 .findFreeSlot
-		addq.w	#6,a2											; otherwise, check next slot
+		addq.w	#6,a2								; otherwise, check next slot
 		tst.l	(a2)
 		bne.s	.findFreeSlot
 
 		; RaiseError is only available in DEBUG builds
-		ifdebug	jsr	(Queue_KosPlus_Module_RaiseError).l						; raise an error if there is kosinski plus moduled buffer overflow
+		ifdebug	jsr	(Queue_KosPlus_Module_RaiseError).l			; raise an error if there is kosinski plus moduled buffer overflow
 
-		move.l	a1,(a2)+										; store source address
-		move.w	d2,(a2)+										; store destination VRAM address
+		move.l	a1,(a2)+							; store source address
+		move.w	d2,(a2)+							; store destination VRAM address
 		rts
 
 ; ---------------------------------------------------------------------------
@@ -169,22 +169,22 @@ Queue_KosPlus_Module:
 ; =============== S U B R O U T I N E =======================================
 
 Process_KosPlus_Module_Queue_Init:
-		move.w	(a1)+,d3										; get uncompressed size
+		move.w	(a1)+,d3							; get uncompressed size
 		move.w	d3,d0
-		lsr.w	d3											; division by 2
+		lsr.w	d3								; division by 2
 		rol.w	#4,d0
-		andi.w	#$1F,d0											; get number of complete modules
+		andi.w	#$1F,d0								; get number of complete modules
 		move.w	d0,(KosPlus_modules_left).w
-		andi.w	#$7FF,d3										; get size of last module in words
-		bne.s	.Gotleftover										; branch if it's non-zero
-		subq.w	#1,(KosPlus_modules_left).w								; otherwise decrement the number of modules
-		move.w	#$1000/2,d3										; and take the size of the last module to be $800 words
+		andi.w	#$7FF,d3							; get size of last module in words
+		bne.s	.Gotleftover							; branch if it's non-zero
+		subq.w	#1,(KosPlus_modules_left).w					; otherwise decrement the number of modules
+		move.w	#$1000/2,d3							; and take the size of the last module to be $800 words
 
 .Gotleftover
 		move.w	d3,(KosPlus_last_module_size).w
 		move.w	d2,(KosPlus_module_destination).w
 		move.l	a1,(KosPlus_module_queue).w
-		addq.w	#1,(KosPlus_modules_left).w								; store total number of modules
+		addq.w	#1,(KosPlus_modules_left).w					; store total number of modules
 		rts
 
 ; ---------------------------------------------------------------------------
@@ -196,14 +196,14 @@ Process_KosPlus_Module_Queue_Init:
 
 Set_KosPlus_Bookmark:
 		tst.w	(KosPlus_decomp_queue_count).w
-		bpl.s	.Done											; branch if a decompression wasn't in progress
-		move.l	$42(sp),d0										; check address V-int is supposed to rte to
+		bpl.s	.Done								; branch if a decompression wasn't in progress
+		move.l	$42(sp),d0							; check address V-int is supposed to rte to
 		cmpi.l	#Process_KosPlus_Queue.Main,d0
 		blo.s	.Done
 		cmpi.l	#Process_KosPlus_Queue.Done,d0
 		bhs.s	.Done
 		move.l	d0,(KosPlus_decomp_bookmark).w
-		move.l	#Backup_KosPlus_Registers,$42(sp)							; force V-int to rte here instead if needed
+		move.l	#Backup_KosPlus_Registers,$42(sp)				; force V-int to rte here instead if needed
 
 .Done
 		rts
@@ -217,29 +217,29 @@ Set_KosPlus_Bookmark:
 Process_KosPlus_Queue:
 		tst.w	(KosPlus_decomp_queue_count).w
 		beq.s	Set_KosPlus_Bookmark.Done
-		bpl.s	.Main											; branch if a decompression was interrupted by V-int
+		bpl.s	.Main								; branch if a decompression was interrupted by V-int
 		movem.w	(KosPlus_decomp_stored_registers).w,d0/d2/d4-d7
 		movem.l	(KosPlus_decomp_stored_registers+(2*6)).w,a0-a1/a5
 		move.l	(KosPlus_decomp_bookmark).w,-(sp)
 		move.w	(KosPlus_decomp_stored_SR).w,-(sp)
-		rtr												; restore ccr
+		rtr									; restore ccr
 ; ---------------------------------------------------------------------------
 
 .Main
-		ori.w	#$8000,(KosPlus_decomp_queue_count).w							; set sign bit to signify decompression in progress
+		ori.w	#$8000,(KosPlus_decomp_queue_count).w				; set sign bit to signify decompression in progress
 		movea.l	(KosPlus_decomp_queue).w,a0
 		movea.l	(KosPlus_decomp_destination).w,a1
 		include "Engine/Decompression/Kosinski Plus Internal.asm"
 		move.l	a0,(KosPlus_decomp_queue).w
 		move.l	a1,(KosPlus_decomp_destination).w
-		andi.w	#$7FFF,(KosPlus_decomp_queue_count).w							; clear decompression in progress bit
+		andi.w	#$7FFF,(KosPlus_decomp_queue_count).w				; clear decompression in progress bit
 		subq.w	#1,(KosPlus_decomp_queue_count).w
-		beq.s	.Done											; branch if there aren't any entries remaining in the queue
+		beq.s	.Done								; branch if there aren't any entries remaining in the queue
 		lea	(KosPlus_decomp_queue).w,a0
 		lea	(KosPlus_decomp_queue+8).w,a1
 
 	rept bytesToXcnt(KosPlus_decomp_queue_end-KosPlus_decomp_queue,8)
-		move.l	(a1)+,(a0)+										; otherwise, shift all entries up
+		move.l	(a1)+,(a0)+							; otherwise, shift all entries up
 		move.l	(a1)+,(a0)+
 	endr
 
@@ -248,7 +248,7 @@ Process_KosPlus_Queue:
 ; ---------------------------------------------------------------------------
 
 Backup_KosPlus_Registers:
-		move	sr,(KosPlus_decomp_stored_SR).w
+		move.w	sr,(KosPlus_decomp_stored_SR).w
 		movem.w	d0/d2/d4-d7,(KosPlus_decomp_stored_registers).w
 		movem.l	a0-a1/a5,(KosPlus_decomp_stored_registers+(2*6)).w
 		rts
@@ -260,5 +260,5 @@ Backup_KosPlus_Registers:
 ; =============== S U B R O U T I N E =======================================
 
 Clear_KosPlus_Module_Queue:
-		clearRAM KosPlus_decomp_queue_count, KosPlus_module_queue_end					; clear the KosPlusM bytes
+		clearRAM KosPlus_decomp_queue_count, KosPlus_module_queue_end		; clear the KosPlusM bytes
 		rts
