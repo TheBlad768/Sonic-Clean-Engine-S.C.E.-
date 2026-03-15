89170f8cea257690038a4e55c8944210e7c76164
TheBlad768 <master.metalsonic2014@gmail.com>
2026-03-15 04:23:27 +0400

    Update Spikebonker.asm

--- Objects/Enemies/Spikebonker/Spikebonker.asm
+++ Objects/Enemies/Spikebonker/Spikebonker.asm
@@ -44,9 +44,9 @@ Obj_Spikebonker:
 
 		; swing setup
 		moveq	#$40,d0
-		move.w	d0,objoff_3E(a0)						; set maximum acceleration before "swinging"
+		move.w	d0,swing_ymax(a0)						; set maximum acceleration before "swinging"
 		move.w	d0,y_vel(a0)							; set velocity
-		move.w	#4,objoff_40(a0)						; acceleration
+		move.w	#4,swing_yacc(a0)						; acceleration
 		bclr	#0,state_flags(a0)						; clear up/down swing flag
 
 		; create
