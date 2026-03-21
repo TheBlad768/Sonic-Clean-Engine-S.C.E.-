f3b23a8c0282eca3a69c65feb3f2e4de539f0314
TheBlad768 <master.metalsonic2014@gmail.com>
2026-03-21 10:43:55 +0400

    Update Variables.asm

--- Engine/Variables.asm
+++ Engine/Variables.asm
@@ -143,7 +143,7 @@ Plane_buffer_end
 
 ; sound variables
 Snd_driver_RAM						ds.b $400			; start of RAM for the sound driver data
-Snd_driver_RAM_end					= *
+Snd_driver_RAM_end =					*
 
 ; misc variables
 Game_mode						ds.b 1
