444de77d7ad89c79d18d3a768e738787ae60b8ac
TheBlad768 <master.metalsonic2014@gmail.com>
2026-03-24 22:34:26 +0400

    Update Settings.asm

--- Engine/Settings.asm
+++ Engine/Settings.asm
@@ -27,7 +27,7 @@ AddressSRAM =						0				; 0 = odd+even, 2 = even only, 3 = odd only
 ; assembly options
 DEBUG_PassCheck =					1				; if 1, check the number of assembly passes
 DEBUG_PassError =					0				; if 1, raise an error if there are more than max assembly passes here
-DEBUG_PassMax =						2				; if 1, set the maximum number of assembly passes
+DEBUG_PassMax =						2				; set the maximum number of assembly passes for a pass error
 
 ; error handler debugging options (only available in DEBUG builds)
 DEBUG_ALL =						1				; if 1, enable all debugging checks
