2f9ee25bea698aae0f70f93465a3e5b23be4b132
TheBlad768 <master.metalsonic2014@gmail.com>
2026-06-15 06:30:12 +0400

    Third-party license update

--- Engine/Decompression/Kosinski Plus Internal.asm
+++ Engine/Decompression/Kosinski Plus Internal.asm
@@ -1,3 +1,21 @@
+; ---------------------------------------------------------------------------
+; This is the internal portion of the code, which is shared by KosPlus_Decomp and
+; Process_KosPlus_Queue. DO NOT CALL THIS DIRECTLY
+; ---------------------------------------------------------------------------
+; For format explanation see https://segaretro.org/Kosinski_compression
+; ---------------------------------------------------------------------------
+; Permission to use, copy, modify, and/or distribute this software for any
+; purpose with or without fee is hereby granted.
+;
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+; OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+; ---------------------------------------------------------------------------
+
 	if _KosPlus_LoopUnroll>0
 		moveq	#(1<<_KosPlus_LoopUnroll)-1,d7
 	endif
