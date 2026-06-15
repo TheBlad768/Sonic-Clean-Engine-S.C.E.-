29d25f495da8e02b8cb00f7b38ce56421befe091
TheBlad768 <master.metalsonic2014@gmail.com>
2026-06-15 06:30:12 +0400

    Third-party license update

--- Sound/Engine/Sonic 2 Clone Driver v2.asm
+++ Sound/Engine/Sonic 2 Clone Driver v2.asm
@@ -2,6 +2,19 @@
 ; Sonic 2 Clone Driver v2 (Mega PCM 2.0 version)
 ; See https://github.com/Clownacy/Sonic-2-Clone-Driver-v2
 ; ===========================================================================
+; Copyright (c) 2013-2025 Clownacy
+;
+; Permission to use, copy, modify, and/or distribute this software for any
+; purpose with or without fee is hereby granted.
+;
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+; REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+; AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+; INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+; LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+; OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+; PERFORMANCE OF THIS SOFTWARE.
+; ===========================================================================
 
 	dc.b	"Clownacy's Sonic 2 Clone Driver v2 (v2.8.0.1)"
 	even
