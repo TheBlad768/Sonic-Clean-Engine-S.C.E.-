59e4501ec2e94bd895afaf608033bfd2a073a6d9
TheBlad768 <41790280+TheBlad768@users.noreply.github.com>
2026-09-22 16:40:27 +0400

    Palette Script update

--- Engine/Objects/Palette Script.asm
+++ Engine/Objects/Palette Script.asm
@@ -7,22 +7,26 @@
 Run_PalRotationScript:
 
 		; check disable flag
-		tst.b	(Palette_rotation_disable).w
-		bne.w	locret_85A00
+		tst.b	(Palette_rotation_disable).w					; is flag set?
+		bne.w	.return								; if yes, branch
 		lea	(Palette_rotation_data).w,a1
 
-loc_85994:
+.wait
+
+		; wait
 		move.w	(a1),d0								; load palette displacement to d0
-		beq.s	locret_85A00							; if 0, return
+		beq.s	.return								; if 0, return
 		subq.b	#1,2(a1)							; decrement delay
-		bpl.s	loc_859CA							; if still positive, go to next entry
+		bpl.s	.next								; if still positive, go to next entry
+
+		; run
 		movea.l	4(a1),a2							; load palette script address to a2
 		movea.w	(a2),a3								; load destination address to a3
 		lea	(a2),a4								; copy script ROM address to a4
 		adda.w	d0,a4								; skip to palette data
 		move.w	(a4),d1								; load the first entry to d1
 		bpl.s	.skip								; if positive, it is a normal entry
-		bsr.s	Run_PalRotationScript_Main					; this is a command
+		bsr.s	.main								; this is a command
 
 .skip
 		moveq	#0,d2
@@ -31,6 +35,8 @@ loc_85994:
 .loop
 		move.w	(a4)+,(a3)+							; copy the next color into destination
 		dbf	d2,.loop							; loop for every color
+
+		; set
 		move.w	(a4)+,d0							; load next delay
 		move.b	d0,2(a1)							; save the delay
 		move.l	a4,d0								; copy current script position into d0
@@ -38,48 +44,54 @@ loc_85994:
 		sub.l	d1,d0								; calculate the current displacement
 		move.w	d0,(a1)								; store it back
 
-loc_859CA:
+.next
 		addq.w	#8,a1								; go to next script
-		bra.s	loc_85994							; run the code again
+		bra.s	.wait								; run the code again
 ; ---------------------------------------------------------------------------
 
-Run_PalRotationScript_Main:
+.main
 		move.b	3(a2),d2							; load additional parameter to d2
-		beq.s	loc_859FC							; FE (repeat)
+		beq.s	.restart							; FE (restart)
 		neg.w	d1
 		jmp	.index-4(pc,d1.w)
 ; ---------------------------------------------------------------------------
 
 .index
-		bra.s	loc_859E2							; FC (loop)
+		bra.s	.change								; FC (loop)
 ; ---------------------------------------------------------------------------
 
 		; custom code								; FA (run)
 		addq.b	#1,3(a1)
 		cmp.b	3(a1),d2
-		bhi.s	loc_859FC							; wait for counter to finish
+		bhi.s	.restart							; wait for counter to finish
+
+		; load
 		movea.l	(Palette_rotation_custom).w,a2
 		pea	(a1)								; save a1
 		jsr	(a2)								; run custom routine
 		movea.l	(sp)+,a1							; restore a1
+
+		; next
 		addq.w	#4,sp								; exit
-		bra.s	loc_859CA
+		bra.s	.next
 ; ---------------------------------------------------------------------------
 
-loc_859E2:
+.change
 		addq.b	#1,3(a1)							; add one to counter
 		cmp.b	3(a1),d2							; compare with max counter
-		bhi.s	loc_859FC
+		bhi.s	.restart
+
+		; set
 		move.w	2(a4),d2
 		adda.w	d2,a2
 		move.l	a2,4(a1)							; load new script after counter has finished
 		movea.w	(a2),a3
 		clr.w	2(a1)
 
-loc_859FC:
+.restart
 		lea	4(a2),a4							; start from the beginning of the rotation
 
-locret_85A00:
+.return
 		rts
 
 ; ---------------------------------------------------------------------------
@@ -91,8 +103,8 @@ locret_85A00:
 Run_PalRotationScript2:
 
 		; check disable flag
-		tst.b	(Palette_rotation_disable).w
-		bne.s	.return
+		tst.b	(Palette_rotation_disable).w					; is flag set?
+		bne.s	.return								; if yes, branch
 
 		; wait
 		subq.b	#1,palrotation_frame_timer(a0)					; subtract 1 from frame duration
@@ -134,5 +146,5 @@ Run_PalRotationSetScript:
 		lea	(Palette_rotation_data).w,a2
 		move.l	(a1)+,(a2)+
 		move.l	(a1)+,(a2)+
-		clr.w	(a2)
+		clr.w	(a2)								; end marker
 		rts
